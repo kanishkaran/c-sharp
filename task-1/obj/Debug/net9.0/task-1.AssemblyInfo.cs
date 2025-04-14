ab99379bbdd1791055e87535cc4917413c3ecf5f
Kanishkaran <Kanishkaran456@gmail.com>
2025-04-14 18:18:37 +0530

    Add Images

--- task-1/obj/Debug/net9.0/task-1.AssemblyInfo.cs
+++ task-1/obj/Debug/net9.0/task-1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task-1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ef41dc8fd2828ab333c80ae1cf7b311fedd9379")]
 [assembly: System.Reflection.AssemblyProductAttribute("task-1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task-1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
