e952f4bd49fb6579652440f77b597c3615e4a6db
Kanishkaran <Kanishkaran456@gmail.com>
2025-04-14 13:13:00 +0530

    Objects and Classes

---
+++ task-2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public void Introduce()
+    {
+        Console.WriteLine($"Hi, I'm {Name} and I'm {Age} years old.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Person p1 = new Person { Name = "Alice", Age = 25 };
+        Person p2 = new Person { Name = "Bob", Age = 30 };
+
+        p1.Introduce();
+        p2.Introduce();
+    }
+}
