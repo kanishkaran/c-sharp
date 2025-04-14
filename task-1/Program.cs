685fb3cd6dcbe1c7bcc523c920498e200c1fa02a
Kanishkaran <Kanishkaran456@gmail.com>
2025-04-14 12:22:55 +0530

    Factorial of a Number

---
+++ task-1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter a Number: ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+double fact = 1;
+for (int i = 1; i <= num; i++)
+{
+    fact *= i;
+}
+
+Console.WriteLine($"Factorial of {num} is {fact}");
