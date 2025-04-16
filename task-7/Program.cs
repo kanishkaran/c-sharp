c526ec275c60664e26082c74412ceabfeca6d6da
kanishkaran <kanishkaran456@gmail.com>
2025-04-16 19:51:37 +0530

    Async & Multi Threading

---
+++ task-7/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Program
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            var tasks = new List<Task> 
+            {
+                FetchSomeFiles(),
+                FetchSomeImages()
+            };
+
+            await Task.WhenAll(tasks);
+        }
+
+        static async Task FetchSomeFiles()
+        {   
+            Console.WriteLine("Fetching Information for file...");
+
+            string TextContent = await File.ReadAllTextAsync("./Sample.txt");
+
+            Console.WriteLine($"Fetched Files \n {TextContent}");
+        }
+
+        static async Task FetchSomeImages()
+        {
+            Console.WriteLine("Fetching Some Images..");
+
+            await Task.Delay(2000);
+
+            Console.WriteLine("Images Fetched..");
+        }
+
+    }
+}
